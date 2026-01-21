86245eb2c6aa0d6113837a4202cf06557501d6c9
AlbertoWebDev <{your_email}>
2026-01-21 09:57:20 +0100

    Template completed

--- script.js
+++ script.js
@@ -33,7 +33,14 @@ class Hotel extends Viaje {
 }
 
 class Paquete extends Viaje {
-    
+        constructor(codigo, destino, precio, vuelo, hotel) {
+        super(codigo, destino, precio);
+        this.vuelo = vuelo;
+        this.hotel = hotel;
+    }
+    getInfo() {
+    return `${super.getInfo()}\n - Vuelo: ${this.vuelo.getInfo()}\n - Hotel: ${this.hotel.getInfo()}`;
+    }
 }
 
 class Cliente {
@@ -47,3 +54,27 @@ class Cliente {
     return `Cliente: ${this.nombre} ${this.apellido}, Email: ${this.email}, Teléfono: ${this.telefono}`;
     }
 }
+
+class Reserva {
+        constructor(cliente, viaje) {
+        this.cliente = cliente;
+        this.viaje = viaje;
+    }
+    getResumen() {
+    return `${this.cliente.getResumen()}\nReservó: ${this.viaje.getInfo()}`;
+    }
+}
+
+// Crear instancias
+const cliente1 = new Cliente("Ana", "Pérez", "ana.perez@gmail.com", "123456789");
+const vuelo1 = new Vuelo("V001", "París", 200, "Air France", 2.5);
+const hotel1 = new Hotel("H001", "París", 100, 4, "Doble");
+const paquete1 = new Paquete("P001", "París", 280, vuelo1, hotel1);
+ 
+// Crear una reserva
+const reserva1 = new Reserva(cliente1, paquete1);
+ 
+console.log(cliente1.getResumen());
+console.log(vuelo1.getInfo());
+console.log(paquete1.getInfo());
+console.log(reserva1.getResumen());
